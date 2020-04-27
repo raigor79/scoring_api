97cc1371d112faa08d1315ccb1d865d8809e7c81
Akhmadeev Igor <raigor@mail.ru>
2020-04-27 21:27:16 +0700

    API after review + test_field

---
+++ store.py
@@ -0,0 +1,16 @@
+import pymemcache
+
+class StoreRequest(object):
+    def __init__(self):
+        pass
+
+    def set(self, arg):
+        pass
+
+    def get(self, arg):
+        pass
+
+    def loadcache(self):
+        pass
+
+
